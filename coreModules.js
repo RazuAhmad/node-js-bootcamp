053e1ffde3a81209bad9b73c3b35924a449bb219
RazuAhmad <abdurrahmanrazu2000@gmail.com>
2023-05-13 00:12:43 +0600

    several modules learned

--- coreModules.js
+++ coreModules.js
@@ -1,6 +1,25 @@
 // Path Module::::::::
 
-var path =require('path');
+let path =require('path');
 
-var fileName=path.parse('E:/React Js Practice/7th semester practice session/HK BASED COMPANY/user-profile-server/index.js');
-console.log(fileName);
+let fileName=path.parse('E:/React Js Practice/7th semester practice session/HK BASED COMPANY/user-profile-server/index.js');
+// console.log(fileName);
+
+
+// OS Moudule::::
+let os= require('node:os');
+// console.log("platform", os.cpus());
+
+// File System(FS) Modules:::
+let fs=require('fs');
+
+// Synchronous way::::
+// const data = fs.readFileSync('myfile.txt');
+// console.log(data);
+
+// Asynchronous way:::
+const data =fs.readFile('myfile.txt',(err,data)=>{
+    // console.log(err);
+    console.log(data.toString());
+});
+console.log("Asynchronous hobe eita");
