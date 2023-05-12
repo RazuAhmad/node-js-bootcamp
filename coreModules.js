8e9cafc3d5c077625fab255f377ab4cb62f4a3d8
RazuAhmad <abdurrahmanrazu2000@gmail.com>
2023-05-12 07:23:27 +0600

    core-modules is being introduced now

---
+++ coreModules.js
@@ -0,0 +1,6 @@
+// Path Module::::::::
+
+var path =require('path');
+
+var fileName=path.parse('E:/React Js Practice/7th semester practice session/HK BASED COMPANY/user-profile-server/index.js');
+console.log(fileName);
